147434a2397c817840406f05b657907c0986899d
igprad <ign.aldi.pradana@gmail.com>
2025-03-19 13:24:13 +0700

    chore: try to make it as construct but still getting illegal memory access (wip)

--- search.cu
+++ search.cu
@@ -1,6 +1,7 @@
 #include <cstring>
 #include <filesystem>
 #include <iostream>
+#include <vector>
 
 #include <cuda_runtime.h>
 #include <cuda_runtime_api.h>
@@ -8,21 +9,17 @@
 
 namespace fs = std::filesystem;
 
-/*
- * TODO:
- * 1) Since device kernel cannot handle pointer of pointer (afaik) will try
- *  a) using struct, if not works [ ] -> result : TBD
- *  b) using Cuda Thrust Vector [ ] -> result : TBD
- */
+struct file {
+  char *path;
+};
 
-__device__ int validate_path_target(char *path, char *target) { return 0; }
-
-__global__ void search_target_file(char *paths, char *target_file_name) {
-  int offset_x = threadIdx.x;
-  int offset_y = blockIdx.x;
-  int idx = offset_x + offset_y * offset_x;
-  if (paths[idx] != '\0')
-    printf("Current path element -> %c\n", paths[idx]);
+__global__ void search_target_file(file *files, char *target_file_name) {
+  int idx = blockIdx.x;
+  if (files[idx].path != nullptr) {
+    char *temp;
+    temp = files[idx].path;
+    printf("%c", temp[0]);
+  }
 }
 
 int main(int argc, char **argv) {
@@ -33,36 +30,40 @@ int main(int argc, char **argv) {
   std::cout << "Target directory : " << current_path << std::endl;
   auto dir_iterator = fs::recursive_directory_iterator{current_path};
 
-  std::string all_paths;
+  std::vector<file> files;
+  int file_ctr = 0;
   for (const auto &dir_entry : dir_iterator) {
     auto current_path = dir_entry.path();
     for (auto it = current_path.begin(); it != current_path.end(); ++it) {
-      all_paths.append("&").append(it->string());
+      std::string selected_path = it->string();
+      file selected_file;
+      selected_file.path = (char *)malloc(strlen(selected_path.data()) + 1);
+      strcpy(selected_file.path, selected_path.data());
+      files.push_back(selected_file);
+      file_ctr++;
     }
   }
-  char *all_paths_chr_ptr = all_paths.data();
-  size_t path_size = sizeof(char) * strlen(all_paths_chr_ptr);
-  char *device_paths = (char *)malloc(path_size);
-  cudaMalloc(&device_paths, path_size);
-  cudaMemcpy(device_paths, all_paths_chr_ptr, path_size,
-             cudaMemcpyHostToDevice);
+
+  file *files_ptr = files.data();
+  size_t files_size = sizeof(*files_ptr) * files.size();
+  file *device_files = (file *)malloc(files_size);
+  cudaMalloc(&device_files, files_size);
+  cudaMemcpy(device_files, files_ptr, files_size, cudaMemcpyHostToDevice);
 
   char *target = search_target.data();
-  size_t target_size = sizeof(char) * strlen(target);
+  size_t target_size = sizeof(target);
   char *device_target = (char *)malloc(target_size);
   cudaMalloc(&device_target, target_size);
   cudaMemcpy(device_target, target, target_size, cudaMemcpyHostToDevice);
 
-  search_target_file<<<1000, 1000>>>(device_paths, device_target);
+  search_target_file<<<1000, 1>>>(device_files, device_target);
   cudaDeviceSynchronize();
-
   cudaError_t error = cudaPeekAtLastError();
   if (error != cudaSuccess) {
     std::cout << "Error in kernel code : " << cudaGetErrorName(error) << " -> "
               << cudaGetErrorString(error) << std::endl;
   }
-
-  cudaFree(device_paths);
+  cudaFree(device_files);
   cudaFree(device_target);
   return 0;
 }
